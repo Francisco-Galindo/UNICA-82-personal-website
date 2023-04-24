f5d75dee516988df12f076db07887bef51a7b17a
Francisco-Galindo <paqui10718@gmail.com>
2023-04-24 17:29:49 -0600

    feat: Componente para mostrar un Proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,79 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
+import Proyecto from './components/Proyecto.vue'
 
 
 </script>
 
 <template>
+  <header>
+    <h1 class="text-3xl font-bold">Francisco Galindo</h1>
+  </header>
+
+  <hr>
+  <nav>
+    <button>About</button>
+    <button>Proyectos</button>
+  </nav>
+
+  <div>
+    <Proyecto
+      :titulo="'Mi proyecto'">
+
+      <template v-slot:imagenes>
+        <img class="max-w-xs m-auto" src="https://wallpaperaccess.com/full/275904.jpg" alt="">
+      </template>
+
+      <template v-slot:cuerpo>
+        <p>
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error quo
+         assumenda ipsa nihil architecto sed voluptatum illum explicabo,
+         officiis deleniti nesciunt quibusdam ut. Pariatur quod, autem
+         voluptates tempora amet blanditiis!
+        </p>
+      </template>
+    </Proyecto>
+
+    <Proyecto
+      :titulo="'Otro proyecto'">
+
+      <template v-slot:imagenes>
+        <img class="max-w-xs m-auto" src="https://www.lifewithcats.tv/wp-content/uploads/2017/01/Screen-Shot-2017-01-26-at-5.49.55-PM.png" alt="">
+      </template>
+
+      <template v-slot:cuerpo>
+        <p>
+          Dolor laborum sint voluptate sunt quas, temporibus amet, quae.
+          Cumque harum ratione dicta dignissimos nesciunt Reprehenderit
+          perferendis possimus quod neque incidunt Quae facere ipsa molestias
+          fugit hic porro. Repellat vitae.
+        </p>
+      </template>
+    </Proyecto>
+  </div>
+
+  <footer>
+    <div>Lol</div>
+  </footer>
+
 </template>
 
 <style scoped>
+nav {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+}
+
+header {
+  padding: 1rem;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
 </style>
---
+++ src/components/Proyecto.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+
+defineProps({
+    titulo: Function,
+    imgUrl: String,
+})
+</script>
+
+<template>
+  <div class="proyecto-container mt-4">
+
+    <h1 class="font-extrabold">{{ titulo }}</h1>
+
+    <div class="mt-8">
+      <slot name="imagenes"></slot>
+    </div>
+
+    <div class="adsf mt-8">
+      <slot name="cuerpo"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
