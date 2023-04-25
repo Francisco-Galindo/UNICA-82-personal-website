95d091f3430de2eda734e079335aafa23d679676
Francisco-Galindo <paqui10718@gmail.com>
2023-04-24 18:01:20 -0600

    feat: Componente para 'Sobre mí'

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<script setup>
+console.log('adsf');
+</script>
+
+<template>
+    <h1 class="text-3xl">Sobre mí</h1>
+  <div class="flex gap-5 text-justify mt-4">
+    <img class="max-w-xs" src="https://usercontent1.hubstatic.com/6852008.jpg" alt="">
+    <div>
+      <p>
+      Elit voluptates veritatis quas illum quasi Non dolor expedita nemo
+      repellendus illo Error esse inventore amet ducimus quis id Rem reiciendis
+      deleniti similique eveniet neque placeat. Consequuntur sint harum
+      deleniti.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
