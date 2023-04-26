58ebc1d1a754ece09075e6fdbff997e4d36906a1
Francisco-Galindo <paqui10718@gmail.com>
2023-04-25 21:52:14 -0600

    feat: Links de redes sociales en About

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,8 +4,8 @@ console.log('adsf');
 
 <template>
     <h1 class="text-3xl">Sobre mí</h1>
-  <div class="flex gap-5 text-justify mt-4">
-    <img class="max-w-xs" src="https://usercontent1.hubstatic.com/6852008.jpg" alt="">
+  <div class="flex gap-5 text-justify mt-4 flex-col md:flex-row p-4">
+    <img class="max-w-xs m-auto" src="https://usercontent1.hubstatic.com/6852008.jpg" alt="">
     <div>
       <p>
       Elit voluptates veritatis quas illum quasi Non dolor expedita nemo
@@ -13,7 +13,26 @@ console.log('adsf');
       deleniti similique eveniet neque placeat. Consequuntur sint harum
       deleniti.
       </p>
+
+      <div class="flex gap-5 justify-evenly mt-4">
+        <a href="https://github.com/Francisco-Galindo">
+          <button class="pl-2 pr-2 w-16 h-16 rounded-full bg-green">
+            <img src="/src/assets/github-icon.svg" alt="GitHub">
+          </button>
+        </a>
+        <a href="https://gitlab.com/Francisco-Galindo">
+          <button class="pl-2 pr-2 w-16 h-16 rounded-full bg-yellow">
+            <img src="/src/assets/gitlab-icon.svg" alt="GitLab">
+          </button>
+        </a>
+        <a href="mailto:francisco@franciscogalindo.com">
+          <button class="pl-2 pr-2 w-16 h-16 rounded-full bg-blue">
+            <img src="/src/assets/email-icon.svg" alt="Mail">
+          </button>
+        </a>
+      </div>
     </div>
+
   </div>
 </template>
 
